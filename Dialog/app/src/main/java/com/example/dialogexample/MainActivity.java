ad90a42c2d5f700a73155485d5208575965a7b41
fifabell@naver.com <fifabell@naver.com>
2020-08-12 11:42:43 +0900

    [200812]

---
+++ Dialog/app/src/main/java/com/example/dialogexample/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.dialogexample;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn_dialog;
+    TextView tv_result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_dialog = (Button)findViewById(R.id.btn_dialog);
+        tv_result = (TextView)findViewById(R.id.tv_result);
+
+        btn_dialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // 다이얼로그 필수 항목
+                AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
+                ad.setIcon(R.mipmap.ic_launcher);
+                ad.setTitle("제목");
+                ad.setMessage("yes or no // question");
+
+                final EditText et = new EditText(MainActivity.this);
+                ad.setView(et);
+
+                ad.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String result = et.getText().toString();
+                        tv_result.setText(result);
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                ad.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+
+                ad.show();
+            }
+        });
+
+
+
+
+    }
+}
